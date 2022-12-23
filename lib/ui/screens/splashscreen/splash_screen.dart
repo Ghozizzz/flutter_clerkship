34e38f4dbcf6d4d5d5e0768ecc2d2be429beb5e8
Ya'suf Dany <yasfdany@gmail.com>
2022-12-23 20:49:09 +0700

    Create splash screen

--- lib/ui/screens/splashscreen/splash_screen.dart
+++ lib/ui/screens/splashscreen/splash_screen.dart
@@ -1,9 +1,11 @@
 import 'package:clerkship/config/themes.dart';
-import 'package:clerkship/ui/screens/login/login_screen.dart';
+import 'package:clerkship/r.dart';
 import 'package:clerkship/utils/nav_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:responsive/responsive.dart';
+import 'package:widget_helper/widget_helper.dart';
 
 import '../../../utils/tools.dart';
 
@@ -20,7 +22,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Tools.onViewCreated(() {
       Future.delayed(const Duration(seconds: 1), () {
-        NavHelper.navigateReplace(LoginScreen());
+        // NavHelper.navigateReplace(LoginScreen());
       });
     });
   }
@@ -35,8 +37,37 @@ class _SplashScreenState extends State<SplashScreen> {
       DeviceOrientation.portraitUp,
     ]);
 
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Themes.primary,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+              child: SvgPicture.asset(
+                AssetImages.logo,
+                width: 166.w,
+              ),
+            ),
+            Container(
+              width: 42.w,
+              height: 3,
+              color: Themes.white,
+            ),
+            Text(
+              'LOG BOOK',
+              style: Themes().whiteBold16?.copyWith(letterSpacing: 3),
+            ).addMarginOnly(
+              top: 17.h,
+              bottom: 8.h,
+            ),
+            Text(
+              'FACULTY OF MEDICINE',
+              style: Themes().white12?.copyWith(letterSpacing: 3),
+            ).addMarginBottom(40.h),
+          ],
+        ),
+      ),
     );
   }
 }
